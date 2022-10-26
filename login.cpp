5b17c1f59da4f6553427e79f32e7caec2e4eb161
KamilKrauze <2414951@dundee.ac.uk>
2022-10-26 15:55:36 +0100

    Saving progress from previous session

--- login.cpp
+++ login.cpp
@@ -1,9 +1,49 @@
 #include <iostream>
+#include <vector>
+#include <fstream>
+
+#include "openssl/sha.h"
 #include "authlib.h"
 
+inline void displayLoginScreen(std::string username, std::string pswd, bool &auth) {
+  (void)auth;
+
+  bool runLoop = true;
+
+  while (runLoop != false) {
+    std::cout << "Username: ";
+    std::cin >> username;
+
+    std::cout << "Password: ";
+    std::cin >> pswd;
+
+    if (username != "" && pswd != "") {
+      break;
+      return;
+    }
+  }
+
+  // Hash function call HERE!
+
+}
+
+class Hash {
+  private:
+    
+};
+
 int main() {
-  bool auth = true;
- 
+  bool auth;
+  std::string username = "";
+  std::string pswd = "";
+  std::vector<std::string, std::string> loginPair;
+
+  loginPair.insert("Hello", "Hi");
+
+  displayLoginScreen(username, pswd, auth);
+
   if (auth) authenticated("user");
   else rejected("user");
+
+  return 0;
 }
